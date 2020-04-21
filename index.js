1bcfb50aa7735385bb89a077ebe295325f76293b
Bakugou808 <janu.m.sung@gmail.com>
2020-04-20 17:27:17 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+//   function moveDodgerRight() {
+//     let rightNumbers = dodger.style.right.replace("px", "");
+//     let right = parseInt(rightNumbers, 10);
+   
+//     if (right < 360) {
+//       dodger.style.right = `${left + 1}px`;
+//     }
+//   }
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
