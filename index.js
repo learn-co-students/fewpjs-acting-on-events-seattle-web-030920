cd3e5cca227f2d70fff780018a26fc4acb3b7472
Theouty <aman.david.michael@gmail.com>
2020-04-22 09:11:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+let dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      let leftNumbers = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumbers, 10);
+   
+      dodger.style.left = `${left - 1}px`;
+      moveDodgerLeft();
+    }
+  });
+
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      let leftNumbers = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumbers, 10);
+   
+      dodger.style.left = `${left + 1}px`;
+      moveDodgerLeft();
+    }
+  });
