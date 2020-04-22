9e4c1f8a6da4069e2bd2e9251b0ea1d4aa15806a
Theouty <aman.david.michael@gmail.com>
2020-04-22 09:16:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 let dodger = document.getElementById("dodger")
 dodger.style.backgroundColor = "#FF69B4";
+  function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
 
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
-      let leftNumbers = dodger.style.left.replace("px", "");
-      let left = parseInt(leftNumbers, 10);
-   
-      dodger.style.left = `${left - 1}px`;
       moveDodgerLeft();
+      moveDodgerRight()
     }
   });
 
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
-      let leftNumbers = dodger.style.left.replace("px", "");
-      let left = parseInt(leftNumbers, 10);
-   
-      dodger.style.left = `${left + 1}px`;
-      moveDodgerLeft();
-    }
-  });
+
+
+  
+  
